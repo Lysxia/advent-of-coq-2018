09b55e9281c5329ef34fea90a0030b770fdfd3e1
Lysxia <lysxia@gmail.com>
2018-12-02 17:58:28 -0500

    Start verifying day01_2

--- day01_2.v
+++ day01_2.v
@@ -65,3 +65,85 @@ Definition exec : io_unit := unsafe_run main.
 
 Extraction "day01_2.ml" exec.
 
+
+Section spec.
+
+(* Stream of partial sums: we get the actual frequencies from
+   a stream of frequency changes. *)
+Definition psums : Stream Z -> Stream Z :=
+  Str_scanl Z.add 0%Z.
+
+(*
+(* [all_distinct xs]: the elements of [xs] are all distinct from
+   each other. *)
+Definition all_distinct {A : Type} (xs : list A) : Prop :=
+  forall pre x suf,
+    xs = pre ++ x :: suf ->
+    ~List.In x pre /\ ~List.In x suf.
+*)
+
+(* [dup n xs]: The [n]-th element of [xs] already occured before. *)
+Definition dup {A : Type} (n : nat) (xs : Stream A) : Prop :=
+  List.In (Str_nth n xs) (Str_take n xs).
+
+(* [first_dup n xs]: The [n]-th element is the first duplicate. *)
+Definition first_dup {A : Type} (n : nat) (xs : Stream A) : Prop :=
+  dup n xs /\
+  forall m, m < n -> ~ dup m xs.
+
+(* [main] outputs the value of the first duplicate, if there is one. *)
+Definition correct (main : io_rel Z Z unit) : Prop :=
+  forall zs xs n,
+    cycle zs = Some xs ->
+    first_dup n xs ->
+    rel_spec Z Z main zs [Str_nth n xs].
+
+Fixpoint Str_drop {A : Type} (n : nat) (xs : Stream A) : Stream A :=
+  match n, xs with
+  | O, _ => xs
+  | S n, Cons _ xs => Str_drop n xs
+  end.
+
+Theorem correct_main : correct main.
+Proof.
+  intros zs xs n Hzs Hn.
+  unfold rel_spec.
+  exists (Mk_io_state [] [Str_nth n xs]).
+  split; [| auto].
+  unfold main.
+  exists xs, (Mk_io_state [] []); split.
+  { (* parse_stream *)
+    unfold parse_stream.
+    exists zs, (Mk_io_state [] []); split.
+    { (* read_all *) apply read_all_rel; auto. }
+    { rewrite Hzs; simpl; auto. }
+  }
+  exists (Str_nth n xs), (Mk_io_state [] []); split.
+  { (* search *)
+    unfold search.
+    match goal with
+    | [ |- mfix ?body _ _ _ _ ] =>
+      assert (H : forall
+                 seen pos pre_xs suf_xs s0 i
+                 (Hpre : pre_xs = Str_take i (psums xs))
+                 (Hsuf : suf_xs = Str_drop i xs)
+                 (Hseen : forall x,
+                     ZSet.In x seen <-> List.In x pre_xs)
+                 (Hpos : pos = Str_nth i (psums xs))
+                 ,
+                 mfix body (seen, pos, suf_xs) s0 s0 (Str_nth n xs))
+    end.
+    {
+      admit.
+    }
+    apply H with (pre_xs := []) (i := 0); auto.
+    { intro x.
+      split.
+      - intros Hno_seen; inversion Hno_seen.
+      - contradiction.
+    }
+  }
+  simpl; auto.
+Abort.
+
+End spec.
