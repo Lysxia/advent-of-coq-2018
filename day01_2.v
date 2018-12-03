1f6575688df6d874e1be240656186fd606b9f18d
Lysxia <lysxia@gmail.com>
2018-12-02 19:13:24 -0500

    Correctness proof for day01_2

--- day01_2.v
+++ day01_2.v
@@ -95,20 +95,114 @@ Definition first_dup {A : Type} (n : nat) (xs : Stream A) : Prop :=
 Definition correct (main : io_rel Z Z unit) : Prop :=
   forall zs xs n,
     cycle zs = Some xs ->
-    first_dup n xs ->
-    rel_spec Z Z main zs [Str_nth n xs].
+    first_dup n (psums xs) ->
+    rel_spec Z Z main zs [Str_nth n (psums xs)].
 
-Fixpoint Str_drop {A : Type} (n : nat) (xs : Stream A) : Stream A :=
-  match n, xs with
-  | O, _ => xs
-  | S n, Cons _ xs => Str_drop n xs
+Lemma ZSet_In_add z z' s :
+  ZSet.In z (ZSet.add z' s) <-> z = z' \/ ZSet.In z s.
+Proof.
+  split.
+  - destruct (Z_as_OT.eq_dec z z').
+    + auto.
+    + right; eapply ZSet.add_3; eauto.
+  - intros [].
+    + apply ZSet.add_1; auto.
+    + apply ZSet.add_2; auto.
+Qed.
+
+Lemma or_iff_distrib :
+  forall A B C D, (A <-> C) -> (B <-> D) -> (A \/ B <-> C \/ D).
+Proof.
+  firstorder.
+Qed.
+
+Theorem search_rel (s0 : io_state Z Z) xs n :
+  first_dup n (psums xs) ->
+  search xs s0 s0 (Str_nth n (psums xs)).
+Proof.
+  intros Hdup.
+  unfold search.
+  match goal with
+  | [ |- mfix ?body _ _ _ _ ] =>
+    assert (H : forall
+               seen pos pre_xs suf_xs s0 i
+               (Hi : i <= n)
+               (Hpre : pre_xs = Str_take i (psums xs))
+               (Hsuf : suf_xs = Str_nth_tl i xs)
+               (Hseen : forall x,
+                   ZSet.In x seen <-> List.In x pre_xs)
+               (Hpos : pos = Str_nth i (psums xs))
+             ,
+               lfp_rel1 body (seen, pos, suf_xs) s0 s0 (Str_nth n (psums xs)))
   end.
+  { intros. remember (n - i) as ni eqn:e_ni.
+    generalize dependent seen.
+    generalize dependent pos.
+    generalize dependent pre_xs.
+    generalize dependent suf_xs.
+    generalize dependent s0.
+    generalize dependent i.
+    induction ni as [| ni IH]; intros;
+      apply lfp_rel_fold;
+      destruct suf_xs as [x suf_xs].
+    - assert (i = n); [| subst i].
+      { symmetry in e_ni. apply Nat.sub_0_le in e_ni.
+        apply Nat.le_antisymm; auto. }
+      rewrite ZSet.mem_1.
+      + simpl; auto.
+      + destruct Hdup as [Hdup _].
+        apply Hseen.
+        rewrite Hpos, Hpre.
+        auto.
+    - assert (Hi' : i < n).
+      { apply lt_O_minus_lt.
+        rewrite <- e_ni.
+        apply Nat.lt_0_succ.
+      }
+      assert (Hmem : ZSet.mem pos seen = false).
+      { destruct Hdup as [Hdup Hnodup].
+        apply Bool.not_true_is_false.
+        intro Hmem_contra.
+        apply ZSet.mem_2 in Hmem_contra.
+        eapply Hnodup; eauto.
+        unfold dup.
+        rewrite <- Hpre, <- Hpos.
+        apply Hseen; auto.
+      }
+      rewrite Hmem.
+      eapply IH with (i := S i); auto.
+      + rewrite Nat.sub_succ_r, <- e_ni; auto.
+      + rewrite Str_nth_tl_S, <- Hsuf.
+        reflexivity.
+      + unfold psums.
+        rewrite Str_scanl_S.
+        f_equal; auto.
+        apply (f_equal (@hd _)) in Hsuf; auto.
+      + intro z. rewrite ZSet_In_add.
+        rewrite Hseen.
+        rewrite Str_take_S.
+        rewrite in_app_iff.
+        rewrite or_comm.
+        rewrite <- Hpre.
+        rewrite <- Hpos.
+        simpl.
+        intuition.
+  }
+  simpl in *.
+  apply H with (pre_xs := []) (i := 0); auto.
+  { apply Nat.le_0_l. }
+  { intro x.
+    split.
+    - intros Hno_seen; inversion Hno_seen.
+    - contradiction.
+  }
+Qed.
 
 Theorem correct_main : correct main.
 Proof.
   intros zs xs n Hzs Hn.
   unfold rel_spec.
-  exists (Mk_io_state [] [Str_nth n xs]).
+  exists (Mk_io_state [] [Str_nth n (psums xs)]).
   split; [| auto].
   unfold main.
   exists xs, (Mk_io_state [] []); split.
@@ -118,32 +212,11 @@ Proof.
     { (* read_all *) apply read_all_rel; auto. }
     { rewrite Hzs; simpl; auto. }
   }
-  exists (Str_nth n xs), (Mk_io_state [] []); split.
+  exists (Str_nth n (psums xs)), (Mk_io_state [] []); split.
   { (* search *)
-    unfold search.
-    match goal with
-    | [ |- mfix ?body _ _ _ _ ] =>
-      assert (H : forall
-                 seen pos pre_xs suf_xs s0 i
-                 (Hpre : pre_xs = Str_take i (psums xs))
-                 (Hsuf : suf_xs = Str_drop i xs)
-                 (Hseen : forall x,
-                     ZSet.In x seen <-> List.In x pre_xs)
-                 (Hpos : pos = Str_nth i (psums xs))
-                 ,
-                 mfix body (seen, pos, suf_xs) s0 s0 (Str_nth n xs))
-    end.
-    {
-      admit.
-    }
-    apply H with (pre_xs := []) (i := 0); auto.
-    { intro x.
-      split.
-      - intros Hno_seen; inversion Hno_seen.
-      - contradiction.
-    }
+    apply search_rel; auto.
   }
   simpl; auto.
-Abort.
+Qed.
 
 End spec.
--- lib/stream.v
+++ lib/stream.v
@@ -2,19 +2,48 @@ From Coq Require Import
      List Streams.
 Import ListNotations.
 
-CoFixpoint Str_scanl {A B : Type}
-           (f : B -> A -> B) (y : B) (xs : Stream A) : Stream B :=
-  Cons y (match xs with
-          | Cons x xs => Str_scanl f (f y x) xs
-          end).
-
 Fixpoint Str_take {A : Type} (n : nat) (xs : Stream A) : list A :=
   match n, xs with
   | O, _ => []%list
   | S n, Cons x xs => x :: Str_take n xs
   end.
 
-Lemma scan_nth {A B : Type}
+CoFixpoint Str_scanl {A B : Type}
+           (f : B -> A -> B) (y : B) (xs : Stream A) : Stream B :=
+  Cons y (match xs with
+          | Cons x xs => Str_scanl f (f y x) xs
+          end).
+
+(* Properties *)
+
+Lemma Str_nth_S {A : Type} (n : nat) (xs : Stream A) :
+  Str_nth (S n) xs = Str_nth n (tl xs).
+Proof. auto. Qed.
+
+Lemma Str_nth_tl_S {A : Type} (n : nat) (xs : Stream A) :
+  Str_nth_tl (S n) xs = tl (Str_nth_tl n xs).
+Proof.
+  revert xs; induction n; auto; intros.
+  simpl; rewrite <- IHn; auto.
+Qed.
+
+Lemma Str_take_S {A : Type} (n : nat) (xs : Stream A) :
+  Str_take (S n) xs = Str_take n xs ++ [Str_nth n xs].
+Proof.
+  revert xs; induction n; intros []; auto.
+  simpl; rewrite <- IHn; auto.
+Qed.
+
+Lemma Str_scanl_S {A B : Type} (n : nat) (f : B -> A -> B)
+      (y : B) (xs : Stream A) :
+  Str_nth (S n) (Str_scanl f y xs) =
+  f (Str_nth n (Str_scanl f y xs)) (Str_nth n xs).
+Proof.
+  revert xs; revert y; induction n; intros; destruct xs; cbn; auto.
+  - repeat rewrite Str_nth_S; simpl. rewrite <- IHn; auto.
+Qed.
+
+Lemma Str_scanl_nth {A B : Type}
       (f : B -> A -> B) (y : B) (xs : Stream A)
       (n : nat) :
   Str_nth n (Str_scanl f y xs) = fold_left f (Str_take n xs) y.
