8985179d66a7c951302c773894f7a0abeb010635
Lysxia <lysxia@gmail.com>
2018-12-05 21:36:25 -0500

    Refactor day05, move bitwise operations to simple-io

--- sol/day05_common.v
+++ sol/day05_common.v
@@ -17,36 +17,35 @@ From advent Require Import lib.
 
 Import SimpleIO.
 
+(* Byte-by-byte input. *)
 Instance MonadI_int_IO : MonadI int IO := {
-  read := catch_eof (input_byte stdin);
+  read :=
+    (* Stop reading at the first newline. *)
+    c <- input_byte stdin;;
+    ret (if (c =? int_of_nat 10)%int then
+           None
+         else
+           Some c);
 }.
 
+(* For debugging *)
 Instance MonadO_list_int_IO : MonadO int IO := {
   print := output_byte stdout;
 }.
-
-Parameter lxor : int -> int -> int.
-Extract Inlined Constant lxor => "Pervasives.(lxor)".
-
-Parameter alphanum : int -> bool.
-Extract Constant alphanum => "fun c -> c >= 65".
 
 Definition reactable (c1 c2 : int) : bool :=
   int_eqb (lxor c1 c2)
           (int_of_nat 32).
 
 Definition react_f (stack : list int) (c : int) : list int :=
-  if alphanum c then
-    match stack with
-    | [] => [c]
-    | c' :: stack' =>
-      if reactable c c' then
-        stack'
-      else
-        c :: stack
-    end
-  else
-    stack.
+  match stack with
+  | [] => [c]
+  | c' :: stack' =>
+    if reactable c c' then
+      stack'
+    else
+      c :: stack
+  end.
 
 Definition react (cs : list int) : list int :=
   fold_left react_f cs [].
