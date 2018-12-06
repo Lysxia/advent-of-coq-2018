bd9578275bbbe99a74d6d7ff7a7b3b37c8c9fe28
Lysxia <lysxia@gmail.com>
2018-12-06 17:33:44 -0500

    itree: Show relation between itree_spec and bind

--- lib/itree.v
+++ lib/itree.v
@@ -1,9 +1,10 @@
 From Coq Require Import
-     List.
+     List
+     Relations Morphisms.
 Import ListNotations.
 
 From ITree Require Export
-     ITree OpenSum Eq.UpToTaus Fix.
+     ITree OpenSum Eq.Eq Eq.UpToTaus Fix MorphismsFacts.
 
 From ExtLib Require Import
      Structures.Monad.
@@ -56,3 +57,56 @@ Instance FoldRead_itree (I O : Type) (E : Type -> Type)
       end
     ) s0;
 }.
+
+(* We could generalize [failureE], but we may need to be careful
+   about how instances resolve. *)
+(* Compared to [io_rel], we get determinism for free. *)
+Definition itree_spec' {I O R : Type}
+           (t : itree (ioE I O +' failureE) R)
+           (i : list I) (o : list O) (r : R) : Prop :=
+  forall i' o',
+    eutt (run_io (Mk_io_state (i ++ i') o') _ t)
+         (Ret (Mk_io_state i' (o' ++ o), r)).
+
+Definition itree_spec {I O : Type}
+           (t : itree (ioE I O +' failureE) unit)
+           (i : list I) (o : list O) :=
+  itree_spec' t i o tt.
+
+Instance Proper_run_io {I O R : Type} {E : Type -> Type} s :
+  Proper (eutt ==> eutt)
+         (@run_io I O E s R).
+Proof.
+Admitted.
+
+Instance Proper_itree_spec' {I O R : Type} :
+  Proper (eutt ==> eq ==> eq ==> eq ==> iff)
+         (@itree_spec' I O R).
+Proof.
+  intros t1 t2 Ht i1 i2 [] o1 o2 [] r1 r2 [].
+  split; intros H i o; [rewrite <- Ht | rewrite Ht]; auto.
+Qed.
+
+Lemma spec_bind {I O A B : Type}
+      (a : A)
+      (ia ib : list I)
+      (oa ob : list O)
+      (m : itree _ A) (k : A -> itree _ B)
+      (i : list I) (o : list O) (b : B) :
+  itree_spec' m ia oa a ->
+  itree_spec' (k a) ib ob b ->
+  i = ia ++ ib ->
+  o = oa ++ ob ->
+  itree_spec' (m >>= k) i o b.
+Proof.
+  intros Hm Hk Hi Ho i' o'.
+  unfold itree_spec', run_io, run_state in *.
+  rewrite interp_state_bind.
+  subst. rewrite <- app_assoc.
+  rewrite Hm.
+  rewrite ret_bind.
+  simpl.
+  rewrite Hk.
+  rewrite <- app_assoc.
+  reflexivity.
+Qed.
