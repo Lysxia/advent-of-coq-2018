668b1ba1565f0fbfb40b59cc5f12d52246511da9
Lysxia <lysxia@gmail.com>
2018-12-06 22:55:22 -0500

    itree: spec_fold_read

--- lib/itree.v
+++ lib/itree.v
@@ -63,15 +63,16 @@ Instance FoldRead_itree (I O : Type) (E : Type -> Type)
 (* Compared to [io_rel], we get determinism for free. *)
 Definition itree_spec' {I O R : Type}
            (t : itree (ioE I O +' failureE) R)
-           (i : list I) (o : list O) (r : R) : Prop :=
-  forall i' o',
-    eutt (run_io (Mk_io_state (i ++ i') o') _ t)
-         (Ret (Mk_io_state i' (o' ++ o), r)).
+           (s s' : io_state I O) (r : R) : Prop :=
+  eutt (run_io s _ t)
+       (Ret (s', r)).
 
 Definition itree_spec {I O : Type}
            (t : itree (ioE I O +' failureE) unit)
            (i : list I) (o : list O) :=
-  itree_spec' t i o tt.
+  exists s',
+    itree_spec' t (Mk_io_state i []) s' tt /\
+    output s' = o.
 
 Instance Proper_run_io {I O R : Type} {E : Type -> Type} s :
   Proper (eutt ==> eutt)
@@ -84,29 +85,68 @@ Instance Proper_itree_spec' {I O R : Type} :
          (@itree_spec' I O R).
 Proof.
   intros t1 t2 Ht i1 i2 [] o1 o2 [] r1 r2 [].
-  split; intros H i o; [rewrite <- Ht | rewrite Ht]; auto.
+  unfold itree_spec'. rewrite Ht; reflexivity.
 Qed.
 
 Lemma spec_bind {I O A B : Type}
       (a : A)
-      (ia ib : list I)
-      (oa ob : list O)
+      (s1 s2 s3 : io_state I O)
       (m : itree _ A) (k : A -> itree _ B)
-      (i : list I) (o : list O) (b : B) :
-  itree_spec' m ia oa a ->
-  itree_spec' (k a) ib ob b ->
-  i = ia ++ ib ->
-  o = oa ++ ob ->
-  itree_spec' (m >>= k) i o b.
+      (b : B) :
+  itree_spec' m s1 s2 a ->
+  itree_spec' (k a) s2 s3 b ->
+  itree_spec' (m >>= k) s1 s3 b.
 Proof.
-  intros Hm Hk Hi Ho i' o'.
-  unfold itree_spec', run_io, run_state in *.
+  unfold itree_spec', run_io, run_state.
+  intros Hm Hk.
   rewrite interp_state_bind.
-  subst. rewrite <- app_assoc.
+  subst.
   rewrite Hm.
   rewrite ret_bind.
   simpl.
   rewrite Hk.
-  rewrite <- app_assoc.
   reflexivity.
+Qed.
+
+Lemma spec_fold_read {I O A : Type}
+      (f : A -> I -> A)
+      (s1 s2 : io_state I O) (a0 : A) :
+  s2 = Mk_io_state [] (output s1) ->
+  itree_spec' (fold_read f a0) s1 s2 (fold_left f (input s1) a0).
+Proof.
+  remember (input s1) as i1.
+  revert a0 s2. generalize dependent s1.
+  induction i1; intros.
+  - unfold fold_read, FoldRead_itree.
+    rewrite mfix_unfold.
+    eapply spec_bind. simpl. unfold id.
+    eapply spec_bind.
+    unfold itree_spec', run_io, run_state.
+    unfold get, lift; rewrite interp_state_liftE.
+    simpl. reflexivity.
+    rewrite <- Heqi1.
+    unfold itree_spec', run_io, run_state.
+    rewrite interp_state_ret. reflexivity.
+    simpl.
+    unfold itree_spec', run_io, run_state.
+    rewrite interp_state_ret.
+    destruct s1.
+    simpl in Heqi1. subst. reflexivity.
+  - unfold fold_read, FoldRead_itree.
+    rewrite mfix_unfold.
+    eapply spec_bind. simpl. unfold id.
+    eapply spec_bind.
+    unfold itree_spec', run_io, run_state.
+    unfold get, lift; rewrite interp_state_liftE.
+    simpl. reflexivity.
+    rewrite <- Heqi1.
+    eapply spec_bind.
+    unfold itree_spec', run_io, run_state.
+    unfold put, lift; rewrite interp_state_liftE.
+    simpl. reflexivity.
+    unfold itree_spec', run_io, run_state.
+    rewrite interp_state_ret.
+    reflexivity.
+    simpl.
+    apply IHi1; auto.
 Qed.
